98d9e5f81d345c4a5f4151b671d425c1079757aa
王钦 <569551020@qq.com>
2019-10-15 14:43:01 +0800

    'BottomSheetDialogFragment增加固定死高度比功能'

--- BaseLibrary/src/main/java/com/april/develop/dialog/SupportDialog.kt
+++ BaseLibrary/src/main/java/com/april/develop/dialog/SupportDialog.kt
@@ -1,6 +1,7 @@
 package com.april.develop.dialog
 
 import android.graphics.Color
+import android.graphics.Point
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.DisplayMetrics
@@ -10,15 +11,19 @@ import android.view.ViewGroup
 import android.view.Window
 import androidx.annotation.CallSuper
 import androidx.annotation.LayoutRes
+import androidx.annotation.NonNull
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.DialogFragment
 import com.april.develop.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-
 
 abstract class SupportDialogFragment : DialogFragment() {
 
     @LayoutRes
     abstract fun setDialogLayoutRes(): Int
+
+    abstract override fun onViewCreated(view: View, savedInstanceState: Bundle?)
 
     /**
      *  默认的背景是否透明
@@ -28,7 +33,9 @@ abstract class SupportDialogFragment : DialogFragment() {
     }
 
     /**
-     * 宽度占屏幕的比例
+     * 宽度占窗口宽度的百分比例
+     *
+     * 返回 null 表示自适应
      */
     protected open fun widthPercent(): Float? {
         return 0.8f
@@ -41,9 +48,12 @@ abstract class SupportDialogFragment : DialogFragment() {
         setStyle(STYLE_NO_TITLE, android.R.style.Theme_Material_Dialog_MinWidth)
     }
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    @CallSuper
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         //弹窗时，不要标题
         dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
         return inflater.inflate(setDialogLayoutRes(), container, false)
@@ -52,7 +62,7 @@ abstract class SupportDialogFragment : DialogFragment() {
     @CallSuper
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        //背景透明
+        //背景透明（这个好像没有效果...）
         if (defaultBackgroundTransparent()) {
             dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         }
@@ -83,6 +93,8 @@ abstract class SupportBottomSheetDialogFragment : BottomSheetDialogFragment() {
     @LayoutRes
     abstract fun setDialogLayoutRes(): Int
 
+    abstract override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+
     /**
      *  默认的背景是否透明
      */
@@ -90,23 +102,65 @@ abstract class SupportBottomSheetDialogFragment : BottomSheetDialogFragment() {
         return true
     }
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    /**
+     * 展开时，固定死的高度占整个窗口高度的百分比，此时不允许滑动
+     *
+     * 返回 null 表示不固定高度
+     */
+    protected open fun fixedHeightPercent(): Float? {
+        return null
+    }
+
+    @CallSuper
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         //弹窗时，不要标题
         dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
         return inflater.inflate(setDialogLayoutRes(), container, false)
     }
 
+    private val mBottomSheetBehavior: BottomSheetBehavior<View>? by lazy {
+        (((view?.parent as? View)?.layoutParams as? CoordinatorLayout.LayoutParams)?.behavior) as? BottomSheetBehavior<View>?
+    }
+    private val mBottomSheetBehaviorCallback by lazy {
+        object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(@NonNull bottomSheet: View, newState: Int) {
+                //禁止拖拽，
+                if (newState == BottomSheetBehavior.STATE_DRAGGING) {
+                    //设置为收缩状态
+                    mBottomSheetBehavior?.state = BottomSheetBehavior.STATE_COLLAPSED
+                }
+            }
+
+            override fun onSlide(@NonNull bottomSheet: View, slideOffset: Float) {}
+        }
+    }
+
     @CallSuper
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        //背景透明
-        if (defaultBackgroundTransparent()) {
-            dialog
-                ?.window
-                ?.findViewById<View>(R.id.design_bottom_sheet)
-                ?.setBackgroundResource(android.R.color.transparent)
+        dialog?.window?.let { window ->
+            window.findViewById<View>(R.id.design_bottom_sheet)?.let { rootView ->
+                //背景透明
+                if (defaultBackgroundTransparent()) {
+                    rootView.setBackgroundResource(android.R.color.transparent)
+                }
+                //固定死高度
+                if (fixedHeightPercent() != null) {
+                    rootView.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
+                    rootView.post {
+                        mBottomSheetBehavior?.setBottomSheetCallback(mBottomSheetBehaviorCallback)
+                        //设置高度
+                        val point = Point()
+                        window.windowManager.defaultDisplay.getSize(point)
+                        mBottomSheetBehavior?.peekHeight =
+                            ((point.y) * fixedHeightPercent()!!).toInt()
+                    }
+                }
+            }
         }
     }
 

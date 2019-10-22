d1cbc1065177e2cc7f8dbb347708263c3718088b
王钦 <569551020@qq.com>
2019-10-22 10:45:39 +0800

    网格布局Item边框修饰增加横纵两个方向边距单独设置

--- MultipleItem/src/main/java/com/april/multiple/GridSpanDecoration.kt
+++ MultipleItem/src/main/java/com/april/multiple/GridSpanDecoration.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright 2017 Zhihu Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.april.multiple
 
 import android.content.Context
@@ -23,30 +8,37 @@ import androidx.recyclerview.widget.RecyclerView
 /**
  * 在 网格 展示下，给 item 之间设置间隔
  *
- * 注：仅适用于 > 纵向 < 排列的线性布局或者网格布局，对于存在在网格布局下通行展示的 item，是否可用暂时未知。
+ * 注意：仅适用于 线性布局 或者 网格布局，如果网格布局里面存在某些 Item 拉通展示的情况，则不可用。
  *
- * 这代码不是自己写的，而是从网上抄来的……
- * （支持 单独设置横向或者纵向的边距，但是暂不支持对应方向上的包含边缘设置 ——  by  April丶）
+ * 新支持：
+ * 单独设置横向或者纵向的边距，
+ *
+ * ——  by  April丶
  */
 class GridSpanDecoration(
-    private val context: Context,
+
+    context: Context,
+    //横向上的间隔（单位 dp）
+    mHorizontalSpacingDP: Int = 0,
+    //纵向上的间隔（单位 dp）
+    mVerticalSpacingDP: Int = 0,
     //网格每行（竖向展示）或者每列（横向展示）展示的个数
-    private val mSpanCount: Int,
-    //横向间隔（单位 dp）
-    private val horizontalSpacingDP: Int,
-    //纵向间隔（单位 dp）
-    private val verticalSpacingDP: Int,
+    private val mSpanCount: Int = 1,
+    //布局方向
+    @RecyclerView.Orientation
+    private val mOrientation: Int = RecyclerView.VERTICAL,
     //是否包含边缘（如果为 false，则 item 与 RecyclerView 布局接触的地方不会出现边距，否则会出现）
-    private val mIncludeEdge: Boolean = false
+    private val mIncludeEdge: Boolean = true
+
 ) : RecyclerView.ItemDecoration() {
 
-    //横向间隔（单位 px）
+    //横向间隔（PX）
     private val mHorizontalSpacing by lazy {
-        (horizontalSpacingDP * context.resources.displayMetrics.density + 0.5f).toInt()
+        context.dp2px(mHorizontalSpacingDP)
     }
-    //纵向间隔（单位 px）
+    //纵向间隔（PX）
     private val mVerticalSpacing by lazy {
-        (verticalSpacingDP * context.resources.displayMetrics.density + 0.5f).toInt()
+        context.dp2px(mVerticalSpacingDP)
     }
 
     override fun getItemOffsets(
@@ -55,9 +47,29 @@ class GridSpanDecoration(
         parent: RecyclerView,
         state: RecyclerView.State
     ) {
-        val position = parent.getChildAdapterPosition(view) // item position
-        val column = position % mSpanCount // item column
+        // item position
+        val position = parent.getChildAdapterPosition(view)
+        // item column
+        val column = position % mSpanCount
+        // orientation
+        when (mOrientation) {
+            RecyclerView.VERTICAL -> {
+                verticalOrientation(outRect, position, column)
+            }
+            RecyclerView.HORIZONTAL -> {
+                horizontalOrientation(outRect, position, column)
+            }
+        }
+    }
 
+    /**
+     * 纵向
+     */
+    private fun verticalOrientation(
+        outRect: Rect,
+        position: Int,
+        column: Int
+    ) {
         if (mIncludeEdge) {
             // spacing - column * ((1f / spanCount) * spacing)
             outRect.left = mHorizontalSpacing - column * mHorizontalSpacing / mSpanCount
@@ -78,4 +90,33 @@ class GridSpanDecoration(
             }
         }
     }
+
+    /**
+     * 横向
+     */
+    private fun horizontalOrientation(
+        outRect: Rect,
+        position: Int,
+        column: Int
+    ) {
+        if (mIncludeEdge) {
+            outRect.top = mVerticalSpacing - column * mVerticalSpacing / mSpanCount
+            outRect.bottom = (column + 1) * mVerticalSpacing / mSpanCount
+            if (position < mSpanCount) {
+                outRect.left = mHorizontalSpacing
+            }
+            outRect.right = mHorizontalSpacing
+        } else {
+            outRect.top = column * mVerticalSpacing / mSpanCount
+            outRect.bottom = mVerticalSpacing - (column + 1) * mVerticalSpacing / mSpanCount
+            if (position >= mSpanCount) {
+                outRect.left = mHorizontalSpacing
+            }
+        }
+    }
+
+}
+
+private fun Context.dp2px(dp: Int): Int {
+    return (dp * resources.displayMetrics.density + 0.5f).toInt()
 }

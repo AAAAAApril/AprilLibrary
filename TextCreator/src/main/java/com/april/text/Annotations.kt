51d0b019db834d359cde242e465e29179383cc96
叶开 <569551020@qq.com>
2019-10-26 19:55:54 +0800

    .

---
+++ TextCreator/src/main/java/com/april/text/Annotations.kt
@@ -0,0 +1,44 @@
+package com.april.text
+
+import android.graphics.Typeface
+import androidx.annotation.IntDef
+import androidx.annotation.StringDef
+
+
+/**
+ * 文字样式注解
+ */
+@IntDef(
+    TextCreatorStyle.Normal,
+    TextCreatorStyle.Bold,
+    TextCreatorStyle.Italic,
+    TextCreatorStyle.BoldItalic
+)
+@Retention(AnnotationRetention.SOURCE)
+annotation class TextCreatorStyle {
+    companion object {
+        const val Normal = Typeface.NORMAL
+        const val Bold = Typeface.BOLD
+        const val Italic = Typeface.ITALIC
+        const val BoldItalic = Typeface.BOLD_ITALIC
+    }
+}
+
+/**
+ * 文字字体注解
+ */
+@StringDef(
+    TextCreatorType.Normal,
+    TextCreatorType.Monospace,
+    TextCreatorType.Serif,
+    TextCreatorType.Sans
+)
+@Retention(AnnotationRetention.SOURCE)
+annotation class TextCreatorType {
+    companion object {
+        const val Monospace = "monospace"
+        const val Serif = "serif"
+        const val Sans = "sans"
+        const val Normal = "normal"
+    }
+}

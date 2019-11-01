624ca3931e71258d5cea1a5283737f5e8160612a
王钦 <569551020@qq.com>
2019-11-01 18:06:10 +0800

    '增加Fragment懒加载旧方案，官方的新方案似乎不怎么好用'

--- BaseLibrary/src/main/java/com/april/develop/ui/contract.kt
+++ BaseLibrary/src/main/java/com/april/develop/ui/contract.kt
@@ -1,11 +1,11 @@
 package com.april.develop.ui
 
+import android.app.Activity
 import android.content.Intent
 import androidx.annotation.CallSuper
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
-import kotlin.random.Random
 
 /*
 
@@ -15,22 +15,52 @@ import kotlin.random.Random
 
 fun FragmentActivity.startContractIntent(
     intent: Intent,
-    result: ((resultCode: Int, data: Intent?) -> Unit)? = null
+    result: OnContractResultListener? = null
 ) {
     startContractIntent(supportFragmentManager, intent, result)
 }
 
+fun FragmentActivity.startContractIntent(
+    targetActivityClass: Class<out Activity>,
+    result: OnContractResultListener? = null
+) {
+    startContractIntent(supportFragmentManager, Intent(this, targetActivityClass), result)
+}
+
+inline fun <reified A : Activity> FragmentActivity.startContractIntent(
+    noinline result: OnContractResultListener? = null
+) {
+    startContractIntent(supportFragmentManager, Intent(this, A::class.java), result)
+}
+
 fun Fragment.startContractIntent(
     intent: Intent,
-    result: ((resultCode: Int, data: Intent?) -> Unit)? = null
+    result: OnContractResultListener? = null
 ) {
     startContractIntent(childFragmentManager, intent, result)
+}
+
+fun Fragment.startContractIntent(
+    targetActivityClass: Class<out Activity>,
+    result: OnContractResultListener? = null
+) {
+    startContractIntent(
+        childFragmentManager,
+        Intent(requireActivity(), targetActivityClass),
+        result
+    )
+}
+
+inline fun <reified A : Activity> Fragment.startContractIntent(
+    noinline result: OnContractResultListener? = null
+) {
+    startContractIntent(childFragmentManager, Intent(requireActivity(), A::class.java), result)
 }
 
 fun startContractIntent(
     manager: FragmentManager,
     intent: Intent,
-    result: ((resultCode: Int, data: Intent?) -> Unit)? = null
+    result: OnContractResultListener? = null
 ) {
     ContractController.getInstance(manager).startContractIntent(intent, result)
 }
@@ -57,11 +87,11 @@ class ContractController : Fragment() {
     }
 
     private var contractRequestCode: Int? = null
-    private var contractResultListener: ((resultCode: Int, data: Intent?) -> Unit)? = null
+    private var contractResultListener: OnContractResultListener? = null
 
     fun startContractIntent(
         intent: Intent,
-        resultListener: ((resultCode: Int, data: Intent?) -> Unit)? = null
+        resultListener: OnContractResultListener? = null
     ) {
         contractRequestCode = 0X101
         contractResultListener = resultListener
@@ -72,10 +102,10 @@ class ContractController : Fragment() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == contractRequestCode) {
-            contractResultListener?.apply {
-                invoke(requestCode, data)
-            }
+            contractResultListener?.invoke(resultCode, data)
         }
     }
 
 }
+
+typealias OnContractResultListener = (resultCode: Int, data: Intent?) -> Unit

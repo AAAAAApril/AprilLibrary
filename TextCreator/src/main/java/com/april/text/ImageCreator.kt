1ed7321a7951ca288060cbac605da6a9f0a32725
王钦 <569551020@qq.com>
2019-11-12 11:17:01 +0800

    '富文本库，设置图片，默认垂直居中'

--- TextCreator/src/main/java/com/april/text/ImageCreator.kt
+++ TextCreator/src/main/java/com/april/text/ImageCreator.kt
@@ -14,7 +14,7 @@ class ImageCreator(
     //图片 Drawable
     val imageDrawable: Drawable? = null,
     //图片是否垂直居中于文字，否则为底部对齐
-    val centerVertical: Boolean = false,
+    val centerVertical: Boolean = true,
     //点击事件
     onClick: ((View) -> Unit)? = null
 //图片样式为占位类型，所以需要给个字符用来占位替换，这里用了空白字符 " "

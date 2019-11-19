a361c54cf5a09bd9eef40a005845ab45ebc6a0ed
叶开 <569551020@qq.com>
2019-11-19 23:53:37 +0800

    '.'

--- FragmentPool/src/main/java/com/april/fragmentpool/IActivitySupport.kt
+++ FragmentPool/src/main/java/com/april/fragmentpool/IActivitySupport.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
 
 interface IActivitySupport : ISupport {
 
@@ -13,14 +12,13 @@ interface IActivitySupport : ISupport {
     var rootFragmentAddedToBackStack: Boolean
 
     fun <F> loadRootFragment(
-        manager: FragmentManager,
         @IdRes
         containerViewId: Int,
         rootFragmentClass: Class<F>,
         savedInstanceState: Bundle? = null,
         //这个参数不是给 transition 用的，而只是用来做记录的
         addToBackStack: Boolean = true,
-        whenRootFragmentIsNull: () -> Fragment,
+        whenRootFragmentIsNull: () -> F,
         onRootFragment: ((F) -> Unit)? = null
     ) where F : Fragment,
             F : IFragmentSupport
--- FragmentPool/src/main/java/com/april/fragmentpool/IActivitySupportImpl.kt
+++ FragmentPool/src/main/java/com/april/fragmentpool/IActivitySupportImpl.kt
@@ -4,33 +4,32 @@ import android.os.Bundle
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProviders
 
 class IActivitySupportImpl : IActivitySupport {
 
     override fun getSupport(): FragmentActivity = this as FragmentActivity
 
-    override var recorder: FragmentPoolRecorder =
+    override var fragmentPoolRecorder: FragmentPoolRecorder =
         ViewModelProviders.of(getSupport()).get(FragmentPoolRecorder::class.java)
 
     override var rootFragmentAddedToBackStack: Boolean = true
 
     override fun <F> loadRootFragment(
-        manager: FragmentManager,
         @IdRes
         containerViewId: Int,
         rootFragmentClass: Class<F>,
         savedInstanceState: Bundle?,
         addToBackStack: Boolean,
-        whenRootFragmentIsNull: () -> Fragment,
+        whenRootFragmentIsNull: () -> F,
         onRootFragment: ((F) -> Unit)?
     ) where F : Fragment, F : IFragmentSupport {
         rootFragmentAddedToBackStack = addToBackStack
+        val manager = getSupport().supportFragmentManager
         var rootFragment: Fragment? = manager.findFragmentByTag(rootFragmentClass.name)
         if (rootFragment == null) {
             rootFragment = whenRootFragmentIsNull.invoke()
-            onRootFragment?.invoke(rootFragment as F)
+            onRootFragment?.invoke(rootFragment)
             manager.beginTransaction().let { transition ->
                 transition.add(containerViewId, rootFragment, rootFragmentClass.name)
                 transition.addToBackStack(rootFragmentClass.name)
@@ -40,7 +39,10 @@ class IActivitySupportImpl : IActivitySupport {
     }
 
     override fun onBackPressedInternal() {
-
+        val suortFragmentList = getSupport().supportFragmentManager.fragments.filter {
+            return@filter it is IFragmentSupport
+        }
+        //TODO
     }
 
 }

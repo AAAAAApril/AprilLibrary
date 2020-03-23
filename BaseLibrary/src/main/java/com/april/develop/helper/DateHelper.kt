f4051d6ea8d83d0d842fa17224a954a405452780
王钦 <569551020@qq.com>
2020-03-23 16:41:22 +0800

    增加一个时间格式化协助类

---
+++ BaseLibrary/src/main/java/com/april/develop/helper/DateHelper.kt
@@ -0,0 +1,115 @@
+package com.april.develop.helper
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * 时间相关格式化
+ *
+ * 参考文章：
+ * http://c.biancheng.net/view/878.html
+ */
+object DateHelper {
+
+    //年份，比如 2019
+    //短的，2019 => 19
+    const val YEAR = "yyyy"
+    const val YEAR_SHORT = "yy"
+
+    //月份 数字，比如 01、12
+    //短的，09 => 9，11 => 11
+    const val MONTH_NUMBER = "MM"
+    const val MONTH_NUMBER_SHORT = "M"
+
+    //月份 文字，比如 中：四月，英：April
+    //短的，中文：四月 => 4月，英文：April => Apr
+    const val MONTH_TEXT = "MMMM"
+    const val MONTH_TEXT_SHORT = "MMM"
+
+    //天数，在月份中。比如：29
+    //短的，08 => 8，17 => 17
+    const val DAY_IN_MONTH = "dd"
+    const val DAY_IN_MONTH_SHORT = "d"
+
+    //天数，在年中。比如：132
+    const val DAY_IN_YEAR = "D"
+
+    //星期，比如 中：星期一，英：Monday
+    //短的，中文：星期一 => 周一，英文：Monday => Mon
+    const val WEEK_DAY = "EEEE"
+    const val WEEK_DAY_SHORT = "E"
+
+    //一个月中的第几个星期
+    const val WEEK_IN_MONTH = "W"
+
+    //一年中的第几个星期
+    const val WEEK_IN_YEAR = "w"
+
+    //24小时制，从 0 开始
+    const val HOUR_IN_DAY_24_0 = "HH"
+    const val HOUR_IN_DAY_24_0_SHORT = "H"
+
+    //12小时制，从 0 开始
+    const val HOUR_IN_DAY_12_0 = "KK"
+    const val HOUR_IN_DAY_12_0_SHORT = "K"
+
+    //12小时制，从 1 开始
+    const val HOUR_IN_DAY_12_1 = "hh"
+    const val HOUR_IN_DAY_12_1_SHORT = "h"
+
+    //24小时制，从 1 开始
+    const val HOUR_IN_DAY_24_1 = "kk"
+    const val HOUR_IN_DAY_24_1_SHORT = "k"
+
+    //一天中的时间段标记，中文：上午、下午，英文：AM、PM
+    const val AM_PM = "a"
+
+    //分钟
+    const val MINUTE = "mm"
+    const val MINUTE_SHORT = "m"
+
+    //秒
+    const val SECOND = "ss"
+    const val SECOND_SHORT = "s"
+
+    //毫秒
+    const val MILLI_SECOND = "SSS"
+
+    /**
+     * 默认时间格式样式：2019-12-03 18:32:55
+     */
+    const val FORMAT_STRING_STYLE_DEFAULT_LONG =
+        "${YEAR}-${MONTH_NUMBER}-${DAY_IN_MONTH} ${HOUR_IN_DAY_24_0}:${MINUTE}:${SECOND}"
+
+    /**
+     * 只保留 年月日：2019-12-03
+     */
+    const val FORMAT_STRING_STYLE_DEFAULT_DAY = "${YEAR}-${MONTH_NUMBER}-${DAY_IN_MONTH}"
+
+    /**
+     * 只保留 时分秒：18:32:55
+     */
+    const val FORMAT_STRING_STYLE_DEFAULT_HOUR = "${HOUR_IN_DAY_24_0}:${MINUTE}:${SECOND}"
+
+    /**
+     * 创建 SimpleDateFormat
+     *
+     * @param formatStringStyle 格式化时间字符串的模板，
+     * 可以使用 [DateType] 里面的值，
+     * 参考 [FORMAT_STRING_STYLE_DEFAULT_LONG]
+     * 例如：
+     *
+     *      val format:SimpleDateFormat = DateType.createSimpleDateFormat(
+     *          FORMAT_STRING_STYLE_DEFAULT_LONG,
+     *          Locale.CHINA
+     *      )
+     *
+     */
+    @JvmOverloads
+    @JvmStatic
+    fun createSimpleDateFormat(
+        formatStringStyle: String,
+        locale: Locale = Locale.getDefault()
+    ): SimpleDateFormat = SimpleDateFormat(formatStringStyle, locale)
+
+}
